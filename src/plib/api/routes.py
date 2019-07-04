c40ca3398491a30e20ebd36fd56d8580f950d1fb
Arul <tbone230590@gmail.com>
2019-07-04 14:18:53 +0530

    updated to functiona based views

--- src/plib/api/routes.py
+++ src/plib/api/routes.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import BooksView, BookDetailView
+from . import views
+#from .views import BooksView, BookDetailView
 
 urlpatterns = {
-    path('books/', BooksView.as_view(), name="list_books"),
-    path('books/<int:id>', BookDetailView.as_view(), name="book_details"),
+    path('books/', views.get_all_books, name="list_books"),
+    path('', views.home, name="home"),
+    #path('books/<int:id>', BookDetailView.as_view(), name="book_details"),
 }
